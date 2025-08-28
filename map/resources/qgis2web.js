dc5e7bf36412e0c980b78782631abedc300ac6f2
chimei-saitama <rxb06112@nifty.ne.jp>
2025-08-28 19:34:37 +0900

    'update'

--- map/resources/qgis2web.js
+++ map/resources/qgis2web.js
@@ -9,7 +9,7 @@ var map = new ol.Map({
 });
 
 //initial view - epsg:3857 coordinates if not "Match project CRS"
-map.getView().fit([15542761.013863, 4276744.941272, 15545754.947436, 4279412.932199], map.getSize());
+map.getView().fit([15546082.205837, 4274808.357929, 15546759.904113, 4275166.002781], map.getSize());
 
 ////small screen definition
     var hasTouchScreen = map.getViewport().classList.contains('ol-touch');
